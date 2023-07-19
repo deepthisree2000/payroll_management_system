27352abefcc1055be5839182d79b27d192c37ca9
deepthisree2000 <deepthigummadavally@gmail.com>
2023-07-19 10:39:15 +0530

    commit changes

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # importing libraries
 from employee_details import employee
-from flask import Flask,render_template,jsonify
+from flask import Flask,render_template,jsonify,request
 
 # creating ojects
 app = Flask(__name__)
@@ -8,15 +8,60 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/employee_signup')
+@app.route('/employee_signup',methods = ['GET','POST'])
 def emp_signup():
-    return render_template('signup.html')
+    if request.method=='POST':
+        eid = request.form.get('eid')
+        ename = request.form.get('ename')
+        dptid = request.form.get('dptid')
+        designation = request.form.get('designation')
+        emailid = request.form.get('emailid')
+        contact_no = request.form.get('contact_no')
+        address = request.form.get('address')
+        print(eid,ename,dptid)
+        emp = employee()
+        emp.empinsert(eid=eid,ename=ename,dptid=dptid,designation=designation,emailid=emailid,contact_no=contact_no,address=address)
+        return render_template('message.html')
+    else:
+        return render_template('signup.html')
+@app.route('/employees',methods = ['GET','POST'])
+def show_employees():
+    emp = employee()
+    data = emp.show_employees()
+    return render_template('showemployees.html',employees = data)
+
+@app.route('/attendance',methods = ['GET','POST'])
+def attendance():
+    if request.method=='POST':
+        dptid= request.form.get('dptid')
+        dpt_name = request.form.get('dpt_name')
+        eid = request.form.get('eid')
+        ename = request.form.get('ename')
+        date= request.form.get('date')
+        time_in = request.form.get('time_in')
+        time_out = request.form.get('time_out')
+        print(eid,ename,dptid)
+        emp = employee()
+        emp.attendance(dptid=dptid,dpt_name=dpt_name,eid=eid,ename=ename,date=date,time_in=time_in,time_out=time_out)
+        return jsonify({'message':'successfully inserted'})
+    return render_template('attendance.html')
+@app.route('/salary_details',methods = ['GET','POST'])
+def salary_details():
+    if request.method=='POST':
+        eid=request.form.get('eid')
+        dptid= request.form.get('dptid')
+        account_number=request.form.get('account_number')
+        PAN = request.form.get('PAN')
+        Base_salary = request.form.get('Base_salary')
+        emp = employee()
+        emp.salary_details(eid=eid,dptid=dptid,account_number=account_number,PAN=PAN,Base_salary=Base_salary)
+        return render_template('message.html')
+    return render_template('enter_salary.html')
 
 if __name__ == '__main__':
     app.run()
 
+# creating object for class
 
-emp = employee()
-
-# calling insert function
+#calling insert function
 #emp.empinsert(eid=1,ename="Bhuvan",dptid=11,designation="Manager",emailid="bhuvan@gmail.com",contact_no=9876543210,address="Mumbai")
--- employee_details.py
+++ employee_details.py
@@ -1,15 +1,50 @@
 # importing libraries
 import sqlite3
 # creating the connection
-conn=sqlite3.connect('PMS.db')
-# creating cursor for sql queries
-cur = conn.cursor()
+
 
 class employee:
     def empinsert(self,**k):
+        conn=sqlite3.connect('PMS.db')
+        # creating cursor for sql queries
+        cur = conn.cursor()
         cur.execute(f'''insert into employee_details values
                     ({k['eid']},"{k['ename']}",
                     {k['dptid']},"{k['designation']}","{k['emailid']}",{k['contact_no']},
                     "{k['address']}")''')
+        conn.commit()
+        print("Data has been inserted successfully")
+    def show_employees(self):
+        conn = sqlite3.connect('PMS.db')
+        cur = conn.cursor()
+        cur.execute("select * from employee_details")
+        data = []
+        for i in cur.fetchall():
+            context = {}
+            context['eid']=i[0]
+            context['ename']=i[1]
+            context['dptid']=i[2]
+            context['designation']=i[3]
+            context['emailid']=i[4]
+            context['contact_no']=i[5]
+            context['address']=i[6]
+            data.append(context)
+        return data
+    def attendance(self,**k):
+        conn=sqlite3.connect('PMS.db')
+        # creating cursor for sql queries
+        cur = conn.cursor()
+        cur.execute(f'''insert into attendance values
+                    ({k['dptid']},"{k['dpt_name']}",
+                    {k['eid']},"{k['ename']}","{k['date']}","{k['time_in']}",
+                    "{k['time_out']}")''')
+        conn.commit()
+        print("Data has been inserted successfully")
+    def salary_details(self,**k):
+        conn=sqlite3.connect('PMS.db')
+        cur = conn.cursor()
+        cur.execute(f'''insert into salary_details values
+                    ({k['eid']},{k['dptid']},
+                    {k['account_number']},"{k['PAN']}",{k['Base_salary']})''')
         conn.commit()
         print("Data has been inserted successfully")
